1f5fd997905af17b94162710205341f57666822f
PawelCZ1 <pawelczapran@gmail.com>
2025-07-29 17:22:20 +0200

    Fighter list resize fixed

--- app/fighter-list/index.tsx
+++ app/fighter-list/index.tsx
@@ -1,5 +1,4 @@
 import { ActivityIndicator, SafeAreaView, View, Text } from 'react-native';
-
 import FighterSearchBar from './components/FighterSearchBar';
 import { VerticalSpacer } from '../../components/VerticalSpacer';
 import FighterList from './components/FighterList';
@@ -8,7 +7,6 @@ import { fetchFighters } from '../../services/api';
 import { Fighter } from '../../models/Fighter';
 
 const FighterListScreen = () => {
-
   const [fighters, setFighters] = useState<Fighter[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -38,37 +36,27 @@ const FighterListScreen = () => {
     loadFighters();
   }, []);
 
-  if (loading) {
-    return (
-      <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="white" />
-        <Text className="text-white mt-2">Loading fighters...</Text>
-      </View>
-    );
-  }
-
-  if (error) {
-    return (
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-red-500">{error}</Text>
-      </View>
-    );
-  }
-
   return (
     <SafeAreaView className="flex-1 w-full h-full bg-black">
-      <View className="flex-1 items-center justify-start  pt-6">
+      <View className="flex-1 items-center justify-start pt-6">
         <FighterSearchBar placeholder="Search" value={searchQuery} onChangeText={setSearchQuery}/>
         <VerticalSpacer size={16}/>
-        <FighterList fighters={filteredFighters} />
+
+        {loading ? (
+          <View className="flex-1 justify-center items-center">
+            <ActivityIndicator size="large" color="white" />
+            <Text className="text-white mt-2">Loading fighters...</Text>
+          </View>
+        ) : error ? (
+          <View className="flex-1 justify-center items-center">
+            <Text className="text-red-500">{error}</Text>
+          </View>
+        ) : (
+          <FighterList fighters={filteredFighters} />
+        )}
       </View>
     </SafeAreaView>
   );
-};
-const styles = {
-  container: `items flex-1 justify-center w-full h-full bg-black px-8`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
 };
 
 export default FighterListScreen;
