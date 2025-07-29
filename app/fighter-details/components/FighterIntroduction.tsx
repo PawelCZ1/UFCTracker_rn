301cd868884545dbaf6dff5c18a93d6653d49500
PawelCZ1 <pawelczapran@gmail.com>
2025-07-29 03:02:05 +0200

    image activity indicator

--- app/fighter-details/components/FighterIntroduction.tsx
+++ app/fighter-details/components/FighterIntroduction.tsx
@@ -1,30 +1,43 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ActivityIndicator } from 'react-native';
 import { Fighter } from '../../../models/Fighter';
+import { useState } from 'react';
 
 interface Props {
   fighter: Fighter;
 }
 
 const FighterIntroduction = ({fighter}: Props) => {
+  const [imageLoading, setImageLoading] = useState(true);
+
   return (
     <View className="p-6">
       <View className="flex flex-col gap-4">
         <View className="flex flex-row">
-          <View className="flex flex-col gap-4">
+          <View className="flex-1 flex-col gap-4">
             <Text className="text-4xl font-bold italic text-white">{fighter.firstName}</Text>
             <Text className="text-4xl font-bold italic text-white">{fighter.lastName}</Text>
             <Text className="ml-2 text-white">{fighter.category}</Text>
           </View>
-          <Image
-            source={{ uri: fighter.imageUrl }}
-            resizeMode="contain"
-            className="w-20 h-20"
-          />
+          <View className="flex-1">
+            {imageLoading && (
+                <View className="absolute inset-0 justify-center items-center">
+                  <ActivityIndicator size="large" color="#fff" />
+                </View>
+            )}
+            <Image
+                key={fighter.imageUrl}
+                source={{ uri: fighter.imageUrl }}
+                resizeMode="contain"
+                className="w-full h-60"
+                onLoadStart={() => setImageLoading(true)}
+                onLoadEnd={() => setImageLoading(false)}
+            />
+          </View>
         </View>
         <View className="flex flex-row items-start justify-start gap-4">
-          <Text className="text-gray-600">Dupa 123</Text>
-          <Text className="text-gray-600">Dupa 123</Text>
-          <Text className="text-gray-600">Dupa 123</Text>
+          <Text className="text-gray-600">WINS: {fighter.wins}</Text>
+          <Text className="text-gray-600">LOSSES: {fighter.losses}</Text>
+          <Text className="text-gray-600">DRAWS: {fighter.draws}</Text>
         </View>
       </View>
     </View>
