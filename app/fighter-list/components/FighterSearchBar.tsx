bb7655b991c3c5161450039289a5cb676c6ff737
PawelCZ1 <pawelczapran@gmail.com>
2025-07-30 00:53:24 +0200

    Clear search bar implemented

--- app/fighter-list/components/FighterSearchBar.tsx
+++ app/fighter-list/components/FighterSearchBar.tsx
@@ -1,5 +1,6 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, TouchableOpacity, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { useRef } from 'react';
 
 interface Props {
   placeholder: string;
@@ -8,16 +9,34 @@ interface Props {
 }
 
 const FighterSearchBar = ({placeholder, value, onChangeText}: Props) => {
+  const textInputRef = useRef<TextInput>(null);
+
+  const clearSearch = () => {
+    onChangeText('');
+    textInputRef.current?.blur();
+  };
+
   return (
     <View className={styles.container}>
       <Feather name="search" size={24} color="#a8b5db" />
       <TextInput
+        ref={textInputRef}
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
+        autoCorrect={false}
+        autoCapitalize="none"
+        submitBehavior="blurAndSubmit"
         className={styles.input}
       />
+      <TouchableOpacity
+        onPress={clearSearch}
+        delayPressIn={0}
+        delayPressOut={0}
+      >
+        <Feather name="x" size={24} color="#a8b5db" />
+      </TouchableOpacity>
     </View>
   );
 };
